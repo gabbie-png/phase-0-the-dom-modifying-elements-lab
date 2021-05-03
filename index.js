d7f9ee4b61f13d990e07459b2bb565a9fd36ef19
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-05-03 16:04:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//const element = document.createElement('div'); 
+document.querySelector('#main')
+.remove("main")
+
+//const newHeader = ('h1#victory') 
+//document.body.appendChild(newHeader)
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'YOUR-NAME is the champion'
